bcd7c03221c46728dd29d3af91d23d5b91ca83c2
egor43 <egor43.rus@mail.ru>
2017-01-19 17:29:49 +0300

    Реализована возможность передачи файла.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include <fstream>
+#include <iterator>
 
 using boost::asio::ip::tcp;
 
@@ -14,16 +16,26 @@ int main()
     tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(),2101)); //Прослушиватель для ожидания подкючения и его обработки. Приимает параметры: объект типа io_service и что собственно слушать (любой адрес и порт 444)
     std::cout << "acceptor - OK" << std::endl;
 
+    tcp::socket socket(io_service); //Создаем подключение, которое будет в ВЕЧНОМ цикле крутиться и ждать подключение клиента.
+    std::cout << "socket - OK" << std::endl;
+
+    std::ifstream in_file_stream("test", std::ios::binary); //Создаем файловый поток на чтение файла (бинарно).
+    std::cout << "File_Stream - OK" << std::endl;
+
+    std::vector <char> File_Buffer; //Создаем буфер для файла;
+    std::cout << "File_Stream - OK" << std::endl;
+
+    File_Buffer.insert(std::begin(File_Buffer), std::istreambuf_iterator <char> {in_file_stream}, std::istreambuf_iterator<char>{}); //Заполняем буфер данным файлом, открытым в бинарном режиме
+    std::cout << "File_Buffer inserted - OK" << std::endl;
+
     while(true)
     {
-        tcp::socket socket(io_service); //Создаем подключение, которое будет в ВЕЧНОМ цикле крутиться и ждать подключение клиента.
-        std::cout << "socket - OK" << std::endl;
-
         acceptor.accept(socket); //Слушаем подключение. На сколько я понял, программа тут застрянет пока не дождется подключения клиента.
         std::cout << "Client connected - OK" << std::endl;
 
-        boost::asio::write(socket,boost::asio::buffer("Connect complited")); //пишем в подключенный сокет сообщение.
+        boost::asio::write(socket,boost::asio::buffer(File_Buffer.data(),File_Buffer.size())); //пишем в подключенный сокет буфер (файл).
         std::cout << "Message send - OK" << std::endl;
+        socket.close(); //Закрываем подключение
     }
     return 0;
 }
